f2772f849370289e29923a09c0dbbbd5b162ff3c
Alexis Baltazar <alexisbaltazar@Alexiss-MacBook-Pro.local>
2019-04-09 16:59:00 -0500

    configure database, still has error warning, not sure if connecting or not

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 
+
+require('dotenv').config();
+require('./config/database');
+
 const app = express();
 
 app.use(logger('dev'));
@@ -27,3 +31,5 @@ const port = process.env.PORT || 3001;
 app.listen(port, function() {
   console.log(`Express app running on port ${port}`)
 });
+
+
